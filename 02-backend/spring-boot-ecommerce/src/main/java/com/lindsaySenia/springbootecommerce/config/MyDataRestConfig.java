ca9464fe71cf196a428a0564168d19393f53d7a1
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-07-19 11:29:20 -0400

    exposed entity ids

--- 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/config/MyDataRestConfig.java
@@ -1,15 +1,28 @@
 package com.lindsaySenia.springbootecommerce.config;
 
 import com.lindsaySenia.springbootecommerce.entity.Product;
-import com.lindsaySenia.springbootecommerce.entity.ProductCategory;
+import com.lindsaySenia.springbootecommerce.entity.ProductCategory;import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -28,8 +41,28 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
+        // call internal helper method to expose IDs
+        exposeIds(config);
     }
 
+    private void exposeIds(RepositoryRestConfiguration config) {
 
+        //expose entity ids
 
+        //get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        //create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        //get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+
+        //expose the entity ids for the array
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+        }
+
+    }
 }
