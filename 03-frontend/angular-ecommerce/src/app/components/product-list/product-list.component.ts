cd00cd3be4e4b8df66bdeed4a975e298cb88326e
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-07-20 16:51:27 -0400

    update

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -11,10 +11,17 @@ import { ProductService } from 'src/app/services/product.service';
 
 export class ProductListComponent implements OnInit {
 
-  products!: Product[];
-  currentCategoryId!: number;
-  currentCategoryName!: string;
-  searchMode!: boolean;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  currentCategoryName: string = 'Books';
+  previousCategoryId: number = 1;
+  searchMode: boolean = false;
+
+  //new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
+
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -65,16 +72,35 @@ export class ProductListComponent implements OnInit {
     this.currentCategoryName = 'Books';
   }
 
-  // method is invoked once you subscribe
+  //
+  // Check if we have a different category than previous
+  // Note: Angular will reuse a component if it is currently being viewed
+  //
 
-  //now get the products for the given categoryId
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      //assign results to the Product array
-      data => {
-        this.products = data;
-      }
-    )
-
+  // if we have a different category id than previous
+  // then set thePageNumber back to 1
+  if (this.previousCategoryId != this.currentCategoryId) {
+    this.thePageNumber = 1;
   }
 
+  this.previousCategoryId = this.currentCategoryId;
+
+  console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+  // method is invoked once you subscribe
+  //now get the products for the given categoryId
+    this.productService.getProductListPaginate(this.thePageNumber - 1,
+                                                this.thePageSize,
+                                                this.currentCategoryId)
+                                                .subscribe(this.processResult());
+  }
+
+  processResult() {
+    return (data: any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    };
+  }
 }
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -24,6 +24,17 @@ export class ProductService {
     return this.httpClient.get<Product>(productUrl);
   }
 
+  getProductListPaginate(thePage: number, 
+                          thePageSize: number, 
+                          theCategoryId: number): Observable<GetResponseProducts> {
+    
+    // build URL based on category id, page and size
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   // return an observable that Maps the JSON data from Spring Data REST to Product array
   getProductList(theCategoryId: number): Observable<Product[]> {
     
@@ -62,6 +73,12 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  pages: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
