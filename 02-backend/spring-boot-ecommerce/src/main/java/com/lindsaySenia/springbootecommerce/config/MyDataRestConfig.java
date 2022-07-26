ee259f56785c4f32a93ac6ac01f1f822dbeb9481
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-07-26 10:58:02 -0400

    java rest config updated

--- 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/config/MyDataRestConfig.java
@@ -1,7 +1,9 @@
 package com.lindsaySenia.springbootecommerce.config;
 
+import com.lindsaySenia.springbootecommerce.entity.Country;
 import com.lindsaySenia.springbootecommerce.entity.Product;
 import com.lindsaySenia.springbootecommerce.entity.ProductCategory;
+import com.lindsaySenia.springbootecommerce.entity.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -33,6 +35,8 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         // disable HTTP methods: PUT, POST and DELETE
         disableHttpMethods(Product.class, config, theUnsupportedActions);
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethods(Country.class, config, theUnsupportedActions);
+        disableHttpMethods(State.class, config, theUnsupportedActions);
 
         // call internal helper method to expose IDs
         exposeIds(config);
