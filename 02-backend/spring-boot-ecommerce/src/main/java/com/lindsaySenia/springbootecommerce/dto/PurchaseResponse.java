341403a80efbf3e9a2df2a8c12f0d2c1c8cf984c
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-08-03 22:33:50 -0400

    PurchaseResponse DTO created

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/dto/PurchaseResponse.java
@@ -0,0 +1,11 @@
+package com.lindsaySenia.springbootecommerce.dto;
+
+import lombok.Data;
+
+// send back Java Object as JSON
+
+@Data
+public class PurchaseResponse {
+
+    private String orderTrackingNumber;
+}
