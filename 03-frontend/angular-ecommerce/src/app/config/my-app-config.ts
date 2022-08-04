463f4c0030e24c69a8ece9c20e9e2e9b7bc4224a
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-08-04 16:37:13 -0400

    OIDC configuration complete

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,2 +1,15 @@
-export interface MyAppConfig {
+export default {
+
+    oidc: {
+        //public identifier of client app
+        clientId: '0oa62as84aVBmmHLa5d7',
+        // issuer of tokens, url for authorizing with Okta Auth Server
+        issuer: 'https://dev-33653187.okta.com/oauth2/default',
+        // where to send user once logged in
+        redirectUri: 'http://localhost:4200/login/callback',
+        //provides access to information about user
+        scopes: ['openid', 'profile', 'email']
+    }
+
+
 }
