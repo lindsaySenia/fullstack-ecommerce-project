0aeeb71196199c7eb794e460430e52de5a97156b
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-08-03 22:56:28 -0400

    placeOrder method defined

--- 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/dto/PurchaseResponse.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/dto/PurchaseResponse.java
@@ -1,11 +1,13 @@
 package com.lindsaySenia.springbootecommerce.dto;
 
 import lombok.Data;
+import lombok.NonNull;
 
 // send back Java Object as JSON
 
 @Data
 public class PurchaseResponse {
 
+    @NonNull
     private String orderTrackingNumber;
 }
--- 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/service/CheckoutServiceImpl.java
@@ -3,7 +3,14 @@ package com.lindsaySenia.springbootecommerce.service;
 import com.lindsaySenia.springbootecommerce.dao.CustomerRepository;
 import com.lindsaySenia.springbootecommerce.dto.Purchase;
 import com.lindsaySenia.springbootecommerce.dto.PurchaseResponse;
+import com.lindsaySenia.springbootecommerce.entity.Customer;
+import com.lindsaySenia.springbootecommerce.entity.Order;
+import com.lindsaySenia.springbootecommerce.entity.OrderItem;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
@@ -16,7 +23,39 @@ public class CheckoutServiceImpl implements CheckoutService {
     }
 
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+
+        // retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        // populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        // save to the database
+        customerRepository.save(customer);
+
+        // return a response
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID number (UUID version-4)
+        return UUID.randomUUID().toString();
+
     }
 }
