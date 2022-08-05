a6af22b3292d797ea7df4c67f4b699bd061c1ec7
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-08-04 21:49:04 -0400

    login component imports

--- 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuth } from '@okta/okta-auth-js';
+import OktaSignIn from '@okta/okta-signin-widget';
+
+import myAppConfig from 'src/app/config/my-app-config';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  oktaSignin: any;
+
+  constructor(private oktaAuth: OktaAuth) { 
+
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
