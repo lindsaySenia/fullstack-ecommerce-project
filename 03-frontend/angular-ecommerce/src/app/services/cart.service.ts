7f3568d16a93732d2447eb47dc6832e87290e96d
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-08-07 13:40:05 -0400

    computeCartTotals updated to call persistCartItems

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -77,6 +77,9 @@ export class CartService {
     // log cart data for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
 
+    // persist cart data
+    this.persistCartItems();
+
   }
 
   persistCartItems() {
