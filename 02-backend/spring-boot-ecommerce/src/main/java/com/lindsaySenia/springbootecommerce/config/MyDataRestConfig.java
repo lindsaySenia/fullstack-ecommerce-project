23a33e57e3ac29d16f9c33a0f1736822434a00ea
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-08-07 15:13:37 -0400

    http patch method disabled

--- 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/config/MyDataRestConfig.java
@@ -34,7 +34,8 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST,
+                                              HttpMethod.DELETE, HttpMethod.PATCH};
 
         // disable HTTP methods: PUT, POST and DELETE
         disableHttpMethods(Product.class, config, theUnsupportedActions);
