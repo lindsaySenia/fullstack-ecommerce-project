cb96937b8e01f1910b4b061d50ba1d2a5d419655
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-08-08 09:50:05 -0400

    httpmethods disabled on order repository

--- 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/lindsaySenia/springbootecommerce/config/MyDataRestConfig.java
@@ -1,9 +1,6 @@
 package com.lindsaySenia.springbootecommerce.config;
 
-import com.lindsaySenia.springbootecommerce.entity.Country;
-import com.lindsaySenia.springbootecommerce.entity.Product;
-import com.lindsaySenia.springbootecommerce.entity.ProductCategory;
-import com.lindsaySenia.springbootecommerce.entity.State;
+import com.lindsaySenia.springbootecommerce.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -42,7 +39,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
-
+        disableHttpMethods(Order.class, config, theUnsupportedActions);
         // call internal helper method to expose IDs
         exposeIds(config);
 
