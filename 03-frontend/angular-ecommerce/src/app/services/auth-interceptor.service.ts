b634c28c1ecf64877c82e7bb434ea4a3408addd8
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-08-09 09:34:13 -0400

    authinterceptor service complete

--- 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -1,20 +1,44 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 import { request } from 'http';
-import { from, Observable } from 'rxjs';
+import { from, lastValueFrom, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private oktaAuthService: OktaAuthStateService, 
+              @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(request: HttpRequest<any>, 
+            next: HttpHandler): Observable<HttpEvent<any>> {
     return from(this.handleAccess(request, next));
   }
 
-  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
-    throw new Error('Method not implemented.');
+  private async handleAccess(request: HttpRequest<any>, 
+                             next: HttpHandler): Promise<HttpEvent<any>> {
+  
+    // Only add an access token for secured endpoints
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+
+      // get access token - async call
+      const accessToken = await this.oktaAuth.getAccessToken();
+
+      // clone request and add new header with access token
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer' + accessToken
+        }
+      });
+    }
+
+    // continue to other interceptors in chain
+    // If none, continue to REST API
+    return await lastValueFrom(next.handle(request));
   }
 }
